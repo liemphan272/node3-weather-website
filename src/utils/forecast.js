bca899b7f65dff094f2aea4a5ac082ae3392dd60
liemphan272 <thanhliem270297@gmail.com>
2021-05-01 18:27:21 +0700

    Add more information in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, lon, callback) => {
             callback('Unable to find location. Try another search', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
